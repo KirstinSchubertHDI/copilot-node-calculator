a8870bf18b3a7e40d84bb2ddaf5438aa756e83db
Kirstin Schubert <kirstin.schubert@hdi.de>
2023-09-20 16:36:52 +0200

    adding pow and sqrt

--- api/controller.js
+++ api/controller.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
-exports.calculate = function(req, res) {
-  req.app.use(function(err, _req, res, next) {
+exports.calculate = function (req, res) {
+  req.app.use(function (err, _req, res, next) {
     if (res.headersSent) {
       return next(err);
     }
@@ -12,10 +12,24 @@ exports.calculate = function(req, res) {
 
   // TODO: Add operator
   var operations = {
-    'add':      function(a, b) { return Number(a) + Number(b) },
-    'subtract': function(a, b) { return a - b },
-    'multiply': function(a, b) { return a * b },
-    'divide':   function(a, b) { return a / b },
+    add: function (a, b) {
+      return Number(a) + Number(b);
+    },
+    subtract: function (a, b) {
+      return a - b;
+    },
+    multiply: function (a, b) {
+      return a * b;
+    },
+    divide: function (a, b) {
+      return a / b;
+    },
+    power: function (a, b) {
+      return Math.pow(a, b);
+    },
+    sqrt: function (a) {
+      return Math.sqrt(a);
+    },
   };
 
   if (!req.query.operation) {
@@ -28,16 +42,22 @@ exports.calculate = function(req, res) {
     throw new Error("Invalid operation: " + req.query.operation);
   }
 
-  if (!req.query.operand1 ||
-      !req.query.operand1.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
-      req.query.operand1.replace(/[-0-9e]/g, '').length > 1) {
+  if (
+    !req.query.operand1 ||
+    !req.query.operand1.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
+    req.query.operand1.replace(/[-0-9e]/g, "").length > 1
+  ) {
     throw new Error("Invalid operand1: " + req.query.operand1);
   }
 
-  if (!req.query.operand2 ||
+  if (req.query.operation !== "sqrt") {
+    if (
+      !req.query.operand2 ||
       !req.query.operand2.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
-      req.query.operand2.replace(/[-0-9e]/g, '').length > 1) {
-    throw new Error("Invalid operand2: " + req.query.operand2);
+      req.query.operand2.replace(/[-0-9e]/g, "").length > 1
+    ) {
+      throw new Error("Invalid operand2: " + req.query.operand2);
+    }
   }
 
   res.json({ result: operation(req.query.operand1, req.query.operand2) });
